e2e8394d854b99e652616b42434df08c6953627c
Michael Sjögren <carl.michael.sjogren@gmail.com>
2023-12-29 13:47:20 +0100

    runnable

--- src/main.zig
+++ src/main.zig
@@ -27,20 +27,11 @@ pub fn main() anyerror!void {
         if (deinit_status == .leak) std.testing.expect(false) catch @panic("TEST FAIL");
     }
 
-    // init grid map
-    // load in sample map
-    const buffer = try alloc.alloc(u8, 1024 * 4);
-    defer alloc.free(buffer);
-    const contents = try std.fs.cwd().readFile(
-        "assets/map_sample.csv",
-        buffer,
-    );
-
     raylib.SetConfigFlags(raylib.FLAG_VSYNC_HINT);
     raylib.InitWindow(screenWidth, screenHeight, "Stank");
 
     defer raylib.CloseWindow(); // Close window and OpenGL context
-    const sampleTilemapTexture = raylib.LoadTexture("assets/sample.png");
+    const sampleTilemapTexture = raylib.LoadTexture("assets/samples/small-sample.png");
     defer raylib.UnloadTexture(sampleTilemapTexture);
     const sample_width: i32 = @divFloor(
         @as(i32, @intCast(sampleTilemapTexture.width)),
@@ -52,33 +43,7 @@ pub fn main() anyerror!void {
     );
 
     const sample_size: usize = @intCast(sample_width * sample_height);
-    std.log.debug("contents \n{s}", .{contents});
-    const lineItr = std.mem.splitAny(u8, contents, "\n");
-    _ = lineItr;
-    // load the sample tiles and the information about each tile in the sample.csv
-    std.log.debug("map dim x {d} y {d}", .{ sample_width, sample_height });
-    const index: i32 = 0;
-
-    std.log.debug("INDEX {d}, sample_size size area {d} ", .{ index, sample_size });
-
-    const world_width: u32 = 8;
-    const world_height: u32 = 8;
-    // INITIALIZE WORLD
-    var worldTiles: [world_width * world_height]wfc.Tile = [_]wfc.Tile{.{
-        .tile_id = 0,
-        .tile_type = .floor,
-        .position = .{},
-        .index = 0,
-    }} ** (world_width * world_height);
-    // POPULATE SET WORLD POS OF EACH TILE
-    for (&worldTiles, 0..) |*tile, i| {
-        tile.*.index = i;
-        tile.*.position = wfc.indexToPositionV2(i, world_width, TileSize);
-        tile.*.tile_type = .empty;
-        tile.*.tile_id = @intCast((sample_width) * (sample_height));
-    }
-    // collapse
-    raylib.SetTargetFPS(60); // Set our gafileme to run at 60 frames-per-second
+    _ = sample_size;
     //--------------------------------------------------------------------------------------
     const camera: raylib.Camera2D = .{ .offset = .{
         .x = 0,
@@ -90,13 +55,14 @@ pub fn main() anyerror!void {
     const width: u32 = @intCast(sample_width);
 
     // Main game loop
-    var textureRectPos = wfc.indexToPositionV2(0, width, TileSize);
-    var textureRect: Rectangle = .{
+    const textureRectPos = wfc.indexToPositionV2(0, width, TileSize);
+    const textureRect: Rectangle = .{
         .width = TileSize,
         .height = TileSize,
         .x = textureRectPos.x,
         .y = textureRectPos.y,
     };
+    _ = textureRect;
 
     try loadSampleData(alloc, "assets/samples/small-sample.tmj");
 
@@ -113,30 +79,28 @@ pub fn main() anyerror!void {
         defer raylib.EndDrawing();
         raylib.BeginMode2D(camera);
         defer raylib.EndMode2D();
-        //raylib.DrawTexture(tilemapTexture, 0, 0, raylib.WHITE);
-        for (worldTiles) |tile| {
-            //if (tile.tile_type == .empty) continue;
-            textureRectPos = wfc.indexToPositionV2(
-                tile.tile_id,
-                (width),
-                TileSize,
-            );
-            textureRect.x = textureRectPos.x;
-            textureRect.y = textureRectPos.y;
-            raylib.DrawTextureRec(
-                sampleTilemapTexture,
-                textureRect,
-                tile.position,
-                raylib.WHITE,
-            );
-        }
-        //----------------------------------------------------------------------------------
+        raylib.DrawTexture(sampleTilemapTexture, 0, 0, raylib.WHITE);
+        //for (worldTiles) |tile| {
+        //    //if (tile.tile_type == .empty) continue;
+        //    textureRectPos = wfc.indexToPositionV2(
+        //        tile.tile_id,
+        //        (width),
+        //        TileSize,
+        //    );
+        //    textureRect.x = textureRectPos.x;
+        //    textureRect.y = textureRectPos.y;
+        //    raylib.DrawTextureRec(
+        //        sampleTilemapTexture,
+        //        textureRect,
+        //        tile.position,
+        //        raylib.WHITE,
+        //    );
+        //}
+        ////----------------------------------------------------------------------------------
     }
 }
 
 pub fn loadSampleData(alloc: anytype, path: []const u8) !void {
+    _ = alloc;
     _ = path;
-    var parsed = try std.json.parseFromSlice(u8, alloc, sample_json, .{});
-    defer parsed.deinit();
-    std.log.debug("{}", .{parsed.value});
 }
